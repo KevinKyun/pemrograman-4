4d7d937f1e71a97ac229cff40cdeb402f3e39a44
Kevin <114798157+KevinKyun@users.noreply.github.com>
2023-04-02 23:26:23 +0700

    Add files via upload

--- ClassMember/Mahasiswa.cs
+++ ClassMember/Mahasiswa.cs
@@ -10,83 +10,24 @@ namespace ClassMember
         private string _nama;
         private string _nim;
         private float _ipk;
-        public float _ipkstandar;
-        public float _ipkmaks;
-        public float _ipkdibawah;
 
         //Properti
-
-
-        public float ipkmaks = 4f;
-        public float ipkdibawah = 3f;
-
         public string Nama
         {
-            get
-            {
-                return _nama;
-            } 
-            set
-            {
-                _nama = value;
-            }
+            get { return _nama; }
+            set { _nama = value; }
         }
+
         public string Nim
         {
-            get
-            {
-                return _nim;
-            }
-            set
-            {
-                _nim = value;
-            }
+            get { return _nim; }
+            set { _nim = value; }
         }
+
         public float Ipk
         {
-            get
-            {
-                return _ipk;
-            }
-            set
-            {
-                _ipk = value;
-            }
-        }
-        public float Ipkstandar
-        {
-            get
-            {
-                return _ipkstandar;
-            }
-            set
-            {
-                _ipkstandar = value;
-            }
-        }
-
-        public float Ipkmaks
-        {
-            get
-            {
-                return _ipkmaks;
-            }
-            set
-            {
-                _ipkmaks = value;
-            }
-        }
-
-        public float Ipkdibawah
-        {
-            get
-            {
-                return _ipkdibawah;
-            }
-            set
-            {
-                _ipkdibawah = value;
-            }
+            get { return _ipk; }
+            set { _ipk = value; }
         }
 
 
@@ -97,20 +38,26 @@ namespace ClassMember
             Console.WriteLine("Dengan Nim : {0}", Nim);
             Console.WriteLine("Dengan Ipk : {0}", Ipk);
 
-            if (Ipk > ipkmaks) 
+            if (Ipk < 3)
             {
-                Console.WriteLine("Jangan asal isi ipk");
+                Console.WriteLine("Nilai kamu tidak tuntas");
             }
-            else if (Ipk < ipkdibawah)
+            else if (Ipk >= 3 && Ipk <= 3.5)
             {
-                Console.WriteLine("Kamu bodoh");
+                Console.WriteLine("Kerja Bagus!! Nilai kamu tuntas");
             }
-            else if (Ipk >= ipkmaks && Ipk <= Ipkdibawah)
+            else if (Ipk > 3.5 && Ipk <= 4)
             {
-                Console.WriteLine("Selamat Kamu lulus");
+                Console.WriteLine("Wow!! Kamu hebat");
             }
-            
-
+            else if (Ipk > 4)
+            {
+                Console.WriteLine("Error!!!! Nilai IPK Kamu tidak valid, nilai maksimum IPK adalah 4");
+            }
+            if (Ipk < 1)
+            {
+                Console.WriteLine("Error!!!! Nilai IPK kamu tidak valid, nilai minimum IPK adalah 1");
             }
         }
     }
+}
